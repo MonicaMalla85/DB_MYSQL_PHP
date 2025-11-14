6f284cbc96d3544a694a2d2486cd5ec8e812dfcb
MonicaMalla85 <inychan85@gmail.com>
2025-11-14 15:32:19 +0100

    navbar

---
+++ TRIP_AGENCY/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TRIP_AGENCY</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #227eb3ff;">
+
+
+
+    <div class="container-fluid">
+
+        <a class="navbar-brand d-flex align-items-center" href="index.php">
+        <img src="logo.png" alt="" style="height: 50px; width: auto;"> Trip-Agency
+        </a>
+
+        <button       
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+                    <div class="navbar-nav">
+
+                        <a href="clienti.php" class="nav-link">Clienti</a>
+                        <a href="destinazioni.php" class="nav-link">Destinazione</a>
+                        <a href="prenotazioni.php" class="nav-link">Prenotazioni</a>
+                        <a href="ricerca.php" class="nav-link">Ricerca</a>
+                        <a href="statistiche.php" class="nav-link">Statistiche</a>
+                    </div>
+        </div>
+
+
+
+
+
+    </div>
+
+</nav>
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
